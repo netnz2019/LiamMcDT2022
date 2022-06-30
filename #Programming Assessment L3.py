d39adee675b057192b88b287a3f5806d00715d4e
LiamMcNeill <107654582+LiamMcNeill@users.noreply.github.com>
2022-07-01 09:31:17 +1200

    Create #Programming Assessment L3.py

---
+++ #Programming Assessment L3.py
@@ -0,0 +1,10 @@
+#Programming Assessment L3
+#Liam McNeill
+#SkinTech Finance GUI App
+
+#-------------------------------|Imports|-------------------------------
+import tkinter
+from tkinter import ttk
+
+#-------------------------------|Windows|-------------------------------
+root = tkinter
