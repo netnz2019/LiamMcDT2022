221b34f9403b9b8fb087f8698941adb0450bab70
LiamMcNeill <107654582+LiamMcNeill@users.noreply.github.com>
2022-07-07 08:48:37 +1200

    Update #Programming Assessment L3.py

--- #Programming Assessment L3.py
+++ #Programming Assessment L3.py
@@ -3,8 +3,26 @@
 #SkinTech Finance GUI App
 
 #-------------------------------|Imports|-------------------------------
-import tkinter
+from tkinter import *
 from tkinter import ttk
 
 #-------------------------------|Windows|-------------------------------
-root = tkinter
+root = Tk()
+root.title("Orders")
+
+#-------------------------------|Widgets|-------------------------------
+orders = StringVar()
+orders.set("Orders") #This will make changes to code easier
+
+#Will change pack() to grid() in later version
+OrdersLabel = Label(root, textvariable=orders, justify="center")
+OrdersLabel.pack()
+
+OrdersButton = Button(root, textvariable=orders, justify="left")
+OrdersButton.pack()
+
+AddOrder = Button(root, text="Add Order", justify="left") #will be used to add orders in later version
+AddOrder.pack()
+
+#-------------------------------|Mainloop|-------------------------------
+root.mainloop()
