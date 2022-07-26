1601ea6aec34c6c15a63c973fc394b1a776a0149
LiamMcNeill <107654582+LiamMcNeill@users.noreply.github.com>
2022-07-27 11:29:07 +1200

    Update #Programming Assessment L3.py

--- #Programming Assessment L3.py
+++ #Programming Assessment L3.py
@@ -10,20 +10,64 @@ from tkinter import ttk
 #Orders window
 root = Tk()
 root.title("Orders")
+root.geometry("500x500")#might change this later
 
 #-------------------------------|Widgets|-------------------------------
-orders = StringVar()
-orders.set("Orders") #This will make changes to code easier
-
 #Will change pack() to grid() in later version
-OrdersLabel = Label(root, textvariable=orders, justify='center')
+OrdersLabel = Label(root, text="Orders", justify='center')
 OrdersLabel.pack()
 
-OrdersButton = Button(root, textvariable=orders, justify='left')
-OrdersButton.pack()
+#Create Frame
+order_frame = Frame(root)
+order_frame.pack(pady=10)
 
-AddOrder = Button(root, text="Add Order", justify='left') #will be used to add orders in later version
-AddOrder.pack()
+order_list = Listbox(order_frame, 
+    width=100,
+    height=15,
+    bg="SystemButtonFace",
+    bd=5,
+    fg="#464646",
+    highlightthickness=0,
+    selectbackground="#a6a6a6",
+    activestyle="none"#line under selected product is gone
+    )
+
+order_list.pack(side=LEFT, fill=BOTH)
+
+#Create product list
+Orders = ["Sunscreen", "SunLotion"] #Add list of SkinTech order but for now just have product names for testing
+#Add product list
+for Order in Orders:
+    order_list.insert(END, Order)
+
+#Add scrollbar
+order_scrollbar = Scrollbar(order_frame)
+order_scrollbar.pack(side=RIGHT, fill=BOTH)
+
+#Add scrollbar
+order_list.config(yscrollcommand=order_scrollbar.set)
+order_scrollbar.config(command=order_list.yview)
+
+#create entry box to add products to the list
+order_entry = Entry(root)
+order_entry.pack(pady=20)
+
+#Create a button frame
+button_frame = Frame(root)
+button_frame.pack(pady=20)
+
+#Button Functions
+def add_order():
+    order_list.insert(END, order_entry.get())
+    order_entry.delete(0, END) #Gets rid of whatever is in the entry.
+def delete_order():
+    order_list.delete(ANCHOR)
+
+#Add & delete buttons
+add_button = Button(button_frame, text="Add Order", command=add_order)
+delete_button = Button(button_frame, text="Delete Order", command=delete_order)
+add_button.grid(row=0, column=0)
+delete_button.grid(row=0, column=1, padx=20)
 
 #-------------------------------|Mainloop|-------------------------------
 root.mainloop()
