150d35b54ff24ec3c5b50f2287fe8c1bf47432e2
LiamMcNeill <107654582+LiamMcNeill@users.noreply.github.com>
2022-08-04 10:50:56 +1200

    Started to add a "Save" option and entered code so that gui can read csv file inputs. Need to change csv values to interges.

--- #Programming Assessment L3.py
+++ #Programming Assessment L3.py
@@ -6,6 +6,7 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import dialog
+import csv #better than excel
 
 #-------------------------------|Windows|-------------------------------
 #Orders window
@@ -16,6 +17,15 @@ root.geometry("500x500")#might change this later
 #-------------------------------|Variables|-----------------------------
 OrdersVar = StringVar()
 OrdersVar.set("Orders")
+
+ProductVar = StringVar()
+ProductVar.set("Product") #Will be used when making/adding orders
+
+filepath = 'ProductList.csv'
+File = open(filepath)
+Reader = csv.reader(File)
+Data = list(Reader)
+print(Data)
 
 #-------------------------------|Widgets|-------------------------------
 #Will change pack() to grid() in later version
@@ -34,14 +44,14 @@ order_list = Listbox(order_frame,
     fg="#464646",
     highlightthickness=0,
     selectbackground="#a6a6a6",
-    activestyle="none"#line under selected order is gone
+    activestyle="none" #line under selected order is gone
     )
 
 order_list.pack(side=LEFT, fill=BOTH)
 
-#Create product list
-Orders = ["Sunscreen", "SunLotion"] #Add list of SkinTech order but for now just have product names for testing
-#Add product list
+#Create Order list
+Orders = [] 
+#Add order list
 for Order in Orders:
     order_list.insert(END, Order)
 
@@ -52,7 +62,7 @@ order_scrollbar.pack(side=RIGHT, fill=BOTH)
 order_list.config(yscrollcommand=order_scrollbar.set)
 order_scrollbar.config(command=order_list.yview)
 
-#create entry box to add products to the list
+#create entry box to add orders to the list
 order_entry = Entry(root)
 order_entry.pack(pady=20)
 
@@ -69,7 +79,7 @@ def delete_order():
 
 #Menu functions
 def save_list():
-    pass        
+    pass
 
 #Add & delete buttons
 add_button = Button(button_frame, text="Add Order", command=add_order)
@@ -78,14 +88,13 @@ add_button.grid(row=0, column=0)
 delete_button.grid(row=0, column=1, padx=20)
 
 #Create Menu
-menu1 = Menu(root)
-root.config(menu=menu1)
+my_menu = Menu(root)
+root.config(menu=my_menu)
 
 #Add items to the menu
-file_menu = Menu(menu1, tearoff=False)
-menu1.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="Save", command=save_list)
-#Possibly add a "Save As" option?
+file_menu = Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Save", command=save_list) #Dropdown option
 
 #-------------------------------|Mainloop|------------------------------
 root.mainloop()
